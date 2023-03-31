6510d85392a1dd96b29a9e07f3f20591389c934a
Haojin Li <hl420@duke.edu>
2023-03-31 04:43:20 -0400

    Add gitignore

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict';
 
 var hexo = hexo || {};
-// hexo.extend.filter.register('before_generate', require('./lib/logic'), 10);
 hexo.extend.filter.register('before_post_render', require('./lib/logic'), 10);
